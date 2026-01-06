bc5c357455d39fbf620b412ca8afb6873682e789
Dominik <dominik.gorski.p@gmail.com>
2026-01-07 00:37:52 +0100

    zmiana 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,17 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+    {
+        ignores: ["node_modules/**"],
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+        },
+        rules: {
+            "no-unused-vars": "off",
+            
+            "no-undef": "off", 
+        },
+    },
+];
